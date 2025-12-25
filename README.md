2b89861a548dec3922f8fbbafee34cfff7ebc15f
Kien Khai Duong <jyduongkhai99@gmail.com>
2025-12-25 23:07:16 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Description
+
+A learning project that uses the GitHub REST API to build a command-line interface (CLI) tool.
+It fetches and displays a list of public events performed by a given GitHub user.
+
+Built with Python and the GitHub API.
+
+## Usage
+
+```bash
+python main.py <username>
+```
+
+Example:
+```bash
+python main.py yang-jackie
---
+++ main.py
@@ -0,0 +1,56 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser(prog="github-activity")
+parser.add_argument("username", help="Github username")
+args = parser.parse_args()
+
+print("Username:", args.username)
+
+url = f"https://api.github.com/users/{args.username}/events/public"
+resp = requests.get(url, headers = {"Accept": "application/vnd.github+json",
+                                    "User-Agent": "github-acitivity-cli (learning project)"})
+
+if resp.status_code == 404:
+    print("User not found.")
+    raise SystemExit(1)
+
+if resp.status_code == 403:
+    print("Request forbidden or rate-limited. Try again later!")
+    raise SystemExit(1)
+
+if not resp.ok:
+    print("GitHub API error:", resp.status_code)
+    raise SystemExit(1)
+
+try:
+    data = resp.json()
+except ValueError:
+    raise SystemExit("Response is not valid JSON")
+
+
+def format_event(ev):
+    etype = ev["type"]
+    repo = ev["repo"]["name"]
+    payload = ev["payload"]
+
+    if etype == "PushEvent":
+        return f"Pushed to {repo}"
+    elif etype == "WatchEvent":
+        return f"Starred {repo}"
+    elif etype == "CreateEvent":
+        return f"Created {payload.ref_type} in {repo}"
+    elif etype == "DeleteEvent":
+        return f"Deleted {payload.ref_type} in {repo}"
+    elif etype == "IssueEvent":
+        return f"{payload.ref_type.capitalize()} an issue in {repo}"
+    else: return f"{etype} in {repo}"
+
+if not data: print("This user has no public events.")
+else:   
+    for ev in data[:20]:
+        print("-", format_event(ev))
+
+
+
+
